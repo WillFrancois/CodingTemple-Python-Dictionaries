9e9d4277f8368681e958e50ac75734332fad882c
WillFrancois <williamjcfrancois@gmail.com>
2024-08-28 07:59:26 -0400

    Updated to show modifications to dictionary

--- Real-World Python Dictionary Applications.py
+++ Real-World Python Dictionary Applications.py
@@ -1,7 +1,10 @@
-#Task 1
 restaurant_menu = {
-    "Starters": {"Soup": 5.99},
-    "Main Course": {"Steak": 17.99, "Salmon": 13.99},
-    "Desserts": {"Cake": 4.99, "Ice Cream": 3.99},
-    "Beverages": {"Can Soda": 0.99, "Fountain Drink": 1.99}
+    "Starters": {"Soup": 5.99, "Bruschetta": 6.50},
+    "Main Course": {"Steak": 15.99, "Salmon": 13.99},
+    "Desserts": {"Cake": 4.99, "Ice Cream": 3.99}
 }
+
+#Task 1
+restaurant_menu["Beverages"] = {"Milk": 1.99, "Water": 0.00}
+restaurant_menu["Main Course"]["Steak"] = 17.99
+restaurant_menu["Starters"].pop("Bruschetta")
